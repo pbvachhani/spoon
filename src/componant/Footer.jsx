c2452ffdf7ce76d6749cf17956d364adc5a9bf2d
Parth <parthcivileng@gmail.com>
2023-03-17 09:43:03 +0530

    footer start

---
+++ src/componant/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <div className='footer_section'>
+                <div className='container'>
+                    <div className='row'>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
